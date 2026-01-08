c522d0b6aa411894757e7b02978af9e0c30d2c43
Chris Malone <chrismrulz@hotmail.com>
2026-01-09 09:58:23 +1100

    Fix linux routine by listening in separate thread

--- src/musubi_tuner/utils/interactive_utils.py
+++ src/musubi_tuner/utils/interactive_utils.py
@@ -1,60 +1,84 @@
-import os
 import sys
+import os
+import threading
+import atexit
 import logging
 
 logger = logging.getLogger(__name__)
 
+_save_requested = False
+_input_thread_started = False
+
+def _restore_settings(fd, old_settings):
+    try:
+        import termios
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    except Exception:
+        pass
+
+def _input_listener():
+    global _save_requested
+    
+    try:
+        import tty
+        import termios
+        
+        fd = sys.stdin.fileno()
+
+        if not os.isatty(fd):
+            return
+
+        old_settings = termios.tcgetattr(fd)
+        atexit.register(_restore_settings, fd, old_settings) # restores original terminal settings on exit
+        tty.setcbreak(fd)
+        
+        while True:
+            char = sys.stdin.read(1)
+            
+            if char.lower() == 's':
+                logger.info("\nSave requested via 's' key")
+                _save_requested = True
+
+            if char == '\x03': # restores ctrl+c behaviour
+                _restore_settings(fd, old_settings)
+                import signal
+                os.kill(os.getpid(), signal.SIGINT)
+                break
+                
+    except Exception:
+        pass
+
+def start_listener():
+    global _input_thread_started
+    if _input_thread_started:
+        return
+
+    try:
+        if sys.stdin.isatty():
+            t = threading.Thread(target=_input_listener, daemon=True)
+            t.start()
+            _input_thread_started = True
+    except Exception:
+        pass
+
 def is_save_requested():
-    """
-    Checks if the 's' key has been pressed in the console.
-    This function is non-blocking and should be called periodically.
-    """
+    global _save_requested
     if os.name == 'nt':
         try:
             import msvcrt
-            if msvcrt.kbhit():
-                key = msvcrt.getch()
-                if key.lower() == b's':
-                    logger.info("Save requested by user (keyboard interrupt 's')")
+            while msvcrt.kbhit():
+                if msvcrt.getch().lower() == b's':
+                    logger.info("Save requested by user")
                     return True
-                # consume other keys
-        except Exception:
+        except:
             pass
+        return False
+
     else:
-        try:
-            import fcntl
-            import termios
-            import tty
-
-            fd = sys.stdin.fileno()
-            if not os.isatty(fd):
-                return False
-
-            old_settings = termios.tcgetattr(fd)
-            old_flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-
-            try:
-                fcntl.fcntl(fd, fcntl.F_SETFL, old_flags | os.O_NONBLOCK)
-                tty.setcbreak(fd)
-                try:
-                    key = sys.stdin.read(1)
-                    if key and key.lower() == 's':
-                        logger.info("Save requested by user (keyboard interrupt 's')")
-                        try:
-                            while sys.stdin.read(1):
-                                pass
-                        except IOError:
-                            pass
-                            
-                        return True
-                except IOError:
-                    pass
-
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-                fcntl.fcntl(fd, fcntl.F_SETFL, old_flags)
-
-        except Exception:
-            pass
-
+        if not _input_thread_started:
+            start_listener()
+        if _save_requested:
+            _save_requested = False
+            return True
+    
     return False
